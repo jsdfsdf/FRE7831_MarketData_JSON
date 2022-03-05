52fd2de97bb0c638443eeac2671374bfd23dbad2
Kuiming Wang <18945879273@163.com>
2022-03-05 00:04:04 -0500

    将B部分的添加进StockPairPrices改到了C部分 D部分添加开根号 添加E部分，把原来的E部分的map变成vector

--- FRE7831_MarketData_JSON/Stock.h
+++ FRE7831_MarketData_JSON/Stock.h
@@ -7,98 +7,6 @@
 #include <utility>
 using namespace std;
 
-//class Trade
-//	 {
-// protected:
-//	 float open;
-//	 float high;
-//	 float low;
-//	 float close;
-//	 int volume;
-// public:
-//	 Trade(float open_, float high_, float low_, float close_, int volume_) :
-//		 open(open_), high(high_), low(low_), close(close_), volume(volume_) {}
-//	 ~Trade() {}
-//	 float GetOpen() const { return open; }
-//	 float GetHigh() const { return high; }
-//	 float GetLow() const { return low; }
-//	 float GetClose() const { return close; }
-//	 int GetVolume() const { return volume; }
-//};
-//
-//
-//class DailyTrade : public Trade
-//	 {
-// private:
-//	 string date;
-//	 float adjusted_close;
-// public:
-//	 DailyTrade(string date_, float open_, float high_, float low_, float close_, float adjusted_close_, int volume_) :
-//		 Trade(open_, high_, low_, close_, volume_), date(date_), adjusted_close(adjusted_close_) {}
-//	 ~DailyTrade() {}
-//	 string GetDate() const { return date; }
-//	 float GetAdjustedClose() const { return close; }
-//	 friend ostream& operator << (ostream& out, const DailyTrade& t)
-//		 {
-//		 out << "Date: " << t.date << " Open: " << t.open << " High: " << t.high << " Low: " << t.low
-//			 << " Close: " << t.close << " Adjusted_Close: " << t.adjusted_close << " Volume: " << t.volume << endl;
-//		 return out;
-//		
-//	}
-//};
-//
-//
-//class IntradayTrade : public Trade
-//	 {
-// private:
-//	 string date;
-//	 string timestamp;
-// public:
-//	 IntradayTrade(string date_, string timestamp_, float open_, float high_, float low_, float close_, int volume_) :
-//		 Trade(open_, high_, low_, close_, volume_), date(date_), timestamp(timestamp_) {}
-//	 ~IntradayTrade() {}
-//	 string GetDate() const { return date; }
-//	 string GetTimestamp() const { return timestamp; }
-//	 friend ostream& operator << (ostream& out, const IntradayTrade& t)
-//		 {
-//		 out << " Date: " << t.date << " Timestamp: " << t.timestamp << " Open: " << t.open
-//			 << " High: " << t.high << " Low: " << t.low << " Close: " << t.close
-//			 << " Volume: " << t.volume << endl;
-//		 return out;
-//		
-//	}
-//	
-//};
-
-//class Stock
-//	 {
-//private:
-//	 string symbol;
-//	 vector<DailyTrade> dailyTrades;
-//	 vector<IntradayTrade> intradayTrades;
-// public:
-//	 Stock() :symbol("") { dailyTrades.clear(); intradayTrades.clear(); }
-//	 Stock(string symbol_) : symbol(symbol_) { dailyTrades.clear(); intradayTrades.clear(); }
-//	 Stock(const Stock& stock) { memcpy(this, &stock, sizeof(stock)); }
-//	 ~Stock() {}
-//	 void addDailyTrade(const DailyTrade& aTrade) { dailyTrades.push_back(aTrade); }
-//	 void addIntradayTrade(const IntradayTrade& aTrade) { intradayTrades.push_back(aTrade); }
-//	 string GetSymbol() const { return symbol; }
-//	 const vector<DailyTrade>& GetDailyTrade(void) const { return dailyTrades; }
-//	 const vector<IntradayTrade>& GetIntradayTrade(void) const { return intradayTrades; }
-//	 friend ostream& operator << (ostream& out, const Stock& s)
-//		 {
-//		 out << "Symbol: " << s.symbol << endl;
-//		 for (vector<DailyTrade>::const_iterator itr = s.dailyTrades.begin(); itr != s.dailyTrades.end(); itr++)
-//			 out << *itr;
-//		 for (vector<IntradayTrade>::const_iterator itr = s.intradayTrades.begin(); itr != s.intradayTrades.end(); itr ++)
-//			 out << *itr;
-//		 return out;
-//		
-//	}
-//	
-//};
-
 class TradeData 
 {
 private:
@@ -205,13 +113,13 @@ struct PairPrice
     PairPrice() : dOpen1(0), dClose1(0), dOpen2(0), dClose2(0), dProfitLoss(0) {}
 
     // Parameterized constructor
-    PairPrice(double dOpen1_, double dClose1_, double dOpen2_, double dClose2_)
+    PairPrice(double dOpen1_, double dClose1_, double dOpen2_, double dClose2_, double dProfitLoss_)
     {
         this->dOpen1 = dOpen1_;
         this->dClose1 = dClose1_;
         this->dOpen2 = dOpen2_;
         this->dClose2 = dClose2_;
-        this->dProfitLoss = 0.;
+        this->dProfitLoss = dProfitLoss_;
     }
 
     friend ostream& operator<<(ostream& out, const PairPrice& pp);
@@ -223,6 +131,7 @@ private:
     pair<string, string> stockPair;
     double volatility;
     double k;
+    double FinalPnL;
     map<string, PairPrice> dailyPairPrices;
 
 public:
@@ -243,6 +152,7 @@ public:
     map<string, PairPrice>& GetDailyPrices() { return dailyPairPrices; }
     double GetVolatility() const { return volatility; }
     double GetK() const { return k; }
+    double GetFinalPnL() const { return FinalPnL; }
 
     // Calculate the total profit and loss
     double GetTotalPnL() const;
@@ -250,6 +160,8 @@ public:
     // Setter
     void SetVolatility(double volatility_) { this->volatility = volatility_; }
     void SetK(double k_) { this->k = k_; }
+    void SetStockPair(string symbol1, string symbol2) { stockPair.first = symbol1; stockPair.second = symbol2; }
+    void SetFinalPnL(double FinalPnL_) { FinalPnL = FinalPnL_; }
 
     void SetDailyPairPrice(string sDate_, PairPrice pairPrice_)
     {
